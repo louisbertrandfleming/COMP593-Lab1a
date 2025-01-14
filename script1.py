ef47cb1df058eed265fdcd0115782a48c811113a
Louis Bertrand <louis.bertrand@flemingcollege.ca>
2025-01-14 12:54:08 -0500

    Revert "Modify scripts 1 and 2"
    This reverts commit 6c0b04f29b8cbffd4a725ac31b749953cfa3ded3.

--- script1.py
+++ script1.py
@@ -1,5 +1,3 @@
 # This is an example.
-"""This is a triple-quoted string.
-You can use theses in Python as comments."""
 print("This is script 1.")
 print("There are two lines.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
 print("This is script 2.")
-print("This is the next line.")
-# I added a comment at the bottom.
+print("This is the second line.")
